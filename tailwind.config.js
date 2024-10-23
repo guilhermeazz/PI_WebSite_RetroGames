9caa6c16309f8a844a0474793a97d5b0d3cf0cf7
Guilherme Albuquerque Zaparolli <guialbuzapa@gmail.com>
2024-10-23 14:03:00 -0300

    base projeto

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          '1': '7B0A1C',
+          '2': 'B6142C',
+          '3': 'F6723A',
+          '4': 'FFFFFF',
+          '5': '172029'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
